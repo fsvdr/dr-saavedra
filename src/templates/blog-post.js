7a3ab7ff938029a82763cb6f209745fb72f06308
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2019-06-25 15:01:37 -0500

    Implement blog post page template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import styles from './blog-post.module.css';
 import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 export default ({
   data: {
@@ -9,9 +11,33 @@ export default ({
   },
 }) => (
   <Layout>
-    <h1>Hello {frontmatter.title}</h1>
+    <SEO
+      lang="es"
+      title={frontmatter.title}
+      description={frontmatter.description}
+      keywords={frontmatter.tags}
+    />
 
-    <div dangerouslySetInnerHTML={{ __html: html }} />
+    <header className={styles.hero}>
+      <div className={styles.hero__card}>
+        <div className={styles.hero__card__wrapper}>
+          <div className={styles.hero__card__details}>
+            <span className="o-subtitle">Del Blog del Dr. Saavedra</span>
+            <h1>{frontmatter.title}</h1>
+            <time dateTime={frontmatter.date}>{frontmatter.prettyDate}</time>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <main className={styles.post}>
+      <div className="o-section">
+        <div
+          className={`o-section__wrapper ${styles.post__content}`}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </main>
   </Layout>
 );
 
@@ -21,6 +47,10 @@ export const query = graphql`
       html
       frontmatter {
         title
+        date
+        prettyDate: date(formatString: "MMMM DD, YYYY", locale: "es")
+        tags
+        description
       }
     }
   }
