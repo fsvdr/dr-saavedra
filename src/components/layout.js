00f0dce1d8c4a4336b1f4bf6fe8fa850e1e06362
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2019-06-28 16:08:22 -0500

    Ditch custom media queries

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,25 +8,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import '../styles/settings/breakpoints.css';
-import '../styles/settings/typography.css';
-import '../styles/settings/font-face.css';
-import '../styles/settings/color.css';
-import '../styles/settings/spacing.css';
-import '../styles/settings/z-index.css';
-
-import '../styles/generic/reset.css';
-import '../styles/generic/base.css';
-
-import '../styles/elements/block.css';
-import '../styles/elements/inline.css';
-
-import '../styles/objects/flex-grid.css';
-import '../styles/objects/lists.css';
-import '../styles/objects/cards.css';
-import '../styles/objects/branding.css';
-import '../styles/objects/section.css';
-
 import Contact from './Contact';
 import Footer from './Footer';
 
