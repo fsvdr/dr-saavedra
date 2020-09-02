384b2c2ebdcc9710fb8ab57cb217735afd250054
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-01 19:02:45 -0500

    Update timetable with calendar data

--- src/components/office-availability.js
+++ src/components/office-availability.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { addDays, eachDayOfInterval, isToday, isTomorrow, format, isSunday } from 'date-fns';
+import {
+  addDays,
+  eachDayOfInterval,
+  isToday,
+  isTomorrow,
+  format,
+  isSunday,
+  areIntervalsOverlapping,
+  addMinutes,
+} from 'date-fns';
 import { es } from 'date-fns/locale';
 import capitalize from '../utils/capitalize';
+import asyncWrap from '../utils/asyncWrap';
+import { schedule } from '../data/office-schedule.json';
 
 const StyledTable = styled.table`
   min-width: 32rem;
@@ -30,7 +41,7 @@ const StyledTable = styled.table`
 
   & td s {
     color: var(--color-text-subtle);
-    text-decoration-thickness: 2px;
+    text-decoration-thickness: 1px;
 
     &::before,
     &::after {
@@ -61,25 +72,14 @@ const StyledTable = styled.table`
   }
 `;
 
-const OfficeHours = [
-  // From top to bottom: Mon, Tue, Wed, Thu, Fri, Sat
-  ['15:00', '15:30', '16:00', '16:30', '17:00'],
-  ['12:00', '12:30', '13:00', '13:30'],
-  ['10:00', '10:30', '11:00', '11:30'],
-  ['17:30', '18:00', '18:30', '19:00', '19:30'],
-  ['12:00', '12:30', '13:00', '13:30'],
-  ['12:00', '12:30', '13:00', '13:30'],
-];
-
-const getTimetableDateJSX = (day) => {
-  // eslint-disable-next-line prettier/prettier
+const getDateHeaderJSX = (day) => {
   if (isToday(day))
     return (
       <th scope="row" key={day}>
         Hoy
       </th>
     );
-  // eslint-disable-next-line prettier/prettier
+
   if (isTomorrow(day))
     return (
       <th scope="row" key={day}>
@@ -97,7 +97,7 @@ const getTimetableDateJSX = (day) => {
 const getTimetableJSX = ([date, hours = []]) => {
   return (
     <tr key={date}>
-      {getTimetableDateJSX(date)}
+      {getDateHeaderJSX(date)}
 
       {isSunday(date) ? (
         <td colSpan="5">
@@ -106,9 +106,15 @@ const getTimetableJSX = ([date, hours = []]) => {
           </p>
         </td>
       ) : (
-        hours.map((time) => (
+        hours.map(({ time, busy }) => (
           <td key={`${date}${time}`}>
-            <time dateTime={time}>{time}</time>
+            {busy ? (
+              <s>
+                <time dateTime={time}>{time}</time>
+              </s>
+            ) : (
+              <time dateTime={time}>{time}</time>
+            )}
           </td>
         ))
       )}
@@ -116,11 +122,72 @@ const getTimetableJSX = ([date, hours = []]) => {
   );
 };
 
+/**
+ * Given a provided timetable and a list of appointments, return a new timetable
+ * with the hours that overlap with an appointment maked as busy
+ * @param timetable
+ * @param appointments
+ */
+const getAvailabilityTimetable = (timetable, appointments) => {
+  const availabilityTimetable = timetable.map(([date, hours = []], dateIndex) => {
+    const datetime = new Date(date);
+
+    const availabilityRichHours = hours.map(({ time }) => {
+      const [hours, minutes] = time.split(':');
+      datetime.setHours(hours);
+      datetime.setMinutes(minutes);
+
+      const overlappedAppointment = appointments[dateIndex].find((appointment) =>
+        areIntervalsOverlapping(
+          { start: datetime, end: addMinutes(datetime, 30) },
+          { start: new Date(appointment.start), end: new Date(appointment.end) }
+        )
+      );
+
+      return { time, busy: overlappedAppointment !== undefined };
+    });
+
+    return [date, availabilityRichHours];
+  });
+
+  return availabilityTimetable;
+};
+
 const OfficeAvailability = () => {
-  const today = new Date();
-  const fiveDaysFromToday = addDays(today, 5);
-  const timetableDates = eachDayOfInterval({ start: today, end: fiveDaysFromToday });
-  const timetable = timetableDates.map((date) => [date, OfficeHours[date.getDay() - 1]]);
+  const [timetable, setTimetable] = useState([]);
+
+  useEffect(() => {
+    const today = new Date();
+    const fiveDaysFromToday = addDays(today, 5);
+    const timetableDates = eachDayOfInterval({ start: today, end: fiveDaysFromToday });
+    const timetable = timetableDates.map((date) => {
+      const hours = schedule[date.getDay() - 1] || [];
+      // Initialize all office hours as not busy
+      return [date, hours.map((hour) => ({ time: hour, busy: false }))];
+    });
+
+    // Set our "totally-not-busy" timetable as soon as possible, if everything goes right with
+    // fetching current appointments we'll update it then
+    setTimetable(timetable);
+
+    const getAppointments = async () => {
+      const [error, response] = await asyncWrap(
+        fetch(
+          `/.netlify/functions/calendar-availability?start=${timetableDates[0].toISOString()}&end=${timetableDates[
+            timetableDates.length - 1
+          ].toISOString()}`
+        )
+      );
+
+      if (error) return;
+
+      const appointments = await response.json();
+
+      setTimetable(getAvailabilityTimetable(timetable, appointments));
+    };
+
+    getAppointments();
+  }, []);
 
   return (
     <StyledTable>
