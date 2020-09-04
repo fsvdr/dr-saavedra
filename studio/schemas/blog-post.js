b9a3a1ef6a421ecfcca2a0080cd8a8e1437c66b9
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-04 15:14:19 -0500

    Add blog post schema

---
+++ studio/schemas/blog-post.js
@@ -0,0 +1,83 @@
+export default {
+  title: 'Artículo',
+  name: 'blogPost',
+  type: 'document',
+  fields: [
+    {
+      title: 'Título',
+      name: 'title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'URL',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Fecha de Publicación',
+      name: 'releaseDate',
+      type: 'date',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Resumen',
+      description: 'Breve descripción del tema que trata el artículo',
+      name: 'summary',
+      type: 'text',
+      rows: 2,
+      validation: (Rule) => Rule.required().max(140),
+    },
+    {
+      title: 'Tags',
+      description: 'Palabras clave que ayuden a encontrar este artículo en Internet',
+      name: 'tags',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+      title: 'Contenido',
+      name: 'content',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Subtitle', value: 'h2' },
+            { title: 'Normal', value: 'normal' },
+          ],
+          marks: {
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+          },
+        },
+        {
+          title: 'Imagen',
+          type: 'image',
+          fields: [
+            {
+              title: 'Leyenda',
+              description: '¿Qué se muestra en esta imagen?',
+              name: 'alt',
+              type: 'string',
+              options: {
+                isHighlighted: true,
+              },
+              validation: (Rule) => Rule.required(),
+            },
+          ],
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+  initialValue: {
+    releaseDate: new Date().toISOString(),
+  },
+};
