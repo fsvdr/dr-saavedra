93c40b382eb0e73543b5295bc18946e50ebb3937
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-09 18:49:43 -0500

    Optimize custom font loading

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,25 @@ import React from 'react';
 import SiteNav from './site-nav';
 import { PropTypes } from 'prop-types';
 import Footer from './footer';
+import { useEffect } from 'react';
 
 const Layout = ({ children, isHome }) => {
+  // Use a system font until our custom font is fully loaded in order to avoid
+  // FOIT (Flash of Invisible Text)
+  useEffect(() => {
+    Promise.allSettled([
+      '400 Greycliff CF',
+      'italic 400 Greycliff CF',
+      '500 Greycliff CF',
+      'italic 500 Greycliff CF',
+      '600 Greycliff CF',
+      'italic 600 Greycliff CF',
+    ]).then(() => {
+      console.log('ready');
+      document.body.classList.add('has-custom-font');
+    });
+  }, []);
+
   return (
     <>
       <SiteNav showInternalLinks={isHome} />
