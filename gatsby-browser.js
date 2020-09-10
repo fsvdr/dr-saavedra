88ff803f76d1b139717779acfc5ce2555c930a02
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-09 22:51:16 -0500

    Use custom domain for analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,7 +9,7 @@ export const wrapRootElement = ({ element }) => (
     <Helmet>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
 
-      <script async defer data-domain="drsaavedra.mx" src="https://plausible.io/js/plausible.js"></script>
+      <script async defer data-domain="drsaavedra.mx" src="https://stats.drsaavedra.mx/js/index.js"></script>
     </Helmet>
 
     {element}
