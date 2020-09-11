216f5da82b4299e1e69b1d0f8f1462eb3323683b
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-09-11 17:32:14 -0500

    Implement loading indicator component

--- src/components/distance-to-location.js
+++ src/components/distance-to-location.js
@@ -1,28 +1,14 @@
 import React, { useReducer, useRef } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import useGeolocation from '../hooks/useGeolocation';
 import asyncWrap from '../utils/asyncWrap';
-
-const RotateAnimation = keyframes`
-  from { transform: rotate(0) }
-  to { transform: rotate(360deg) }
-`;
+import LoadingIndicator from './loading-indicator';
 
 const Container = styled.div`
   & p {
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
     font-size: var(--font-size-xs);
     padding: 0.2rem 0;
     margin-block-end: 0;
-  }
-
-  & svg {
-    color: var(--color-accent);
-    margin-inline-end: 0.8rem;
-    margin-block-start: 0;
-    animation: ${RotateAnimation} 2s infinite;
   }
 `;
 
@@ -99,35 +85,7 @@ const DistanceToLocation = () => {
       ) : null}
 
       <p role="status" aria-live="assertive" ref={statusRef} tabIndex={0}>
-        {state.loading ? (
-          <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="icon"
-              focusable="false"
-              aria-hidden="true"
-            >
-              <line x1="12" y1="2" x2="12" y2="6"></line>
-              <line x1="12" y1="18" x2="12" y2="22"></line>
-              <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
-              <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
-              <line x1="2" y1="12" x2="6" y2="12"></line>
-              <line x1="18" y1="12" x2="22" y2="12"></line>
-              <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
-              <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
-            </svg>
-
-            <span>Calculando distancia</span>
-          </>
-        ) : (
-          ''
-        )}
+        {state.loading ? <LoadingIndicator>Calculando distancia</LoadingIndicator> : null}
         {!state.loading && state.error ? state.error : ''}
         {!state.loading && state.travel ? state.travel : ''}
       </p>
